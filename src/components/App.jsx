ff41681af19999d5c89e54fab32fbc4c736cb5fd
gekoparty <gekoparty@hotmail.com>
2021-08-14 21:55:50 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,12 @@
 import '../styles/index.scss';
-import React from 'react';
-import Recipes from './Recipes';
-import sword from '../images/swc-sword.png';
-import swordSvg from '../images/sword.svg';
+
+
 
 const App = () => {
     return (
-        <>
-            <section className="hero"></section>
-            <main>
-                <section>
-                    <h1>Hi there react webpck</h1>
-                </section>
-                <img src={sword} alt="sword" width="250"></img>
-                <img src={swordSvg} alt="sword" width="250"></img>
-                <Recipes></Recipes>
-            </main>
-            
-        </>
+        <div>
+
+        </div>
     )
 }
 
