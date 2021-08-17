e8ea9fd93b54b9e9dbbcd0ebf0735eab186451f3
gekoparty <gekoparty@hotmail.com>
2021-08-17 12:12:07 +0200

    Render App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,9 @@ import '../styles/index.scss';
 const App = () => {
     return (
         <div>
-
+            <div class="container">
+            <p id="name">Pumpkin</p>
+        </div>
         </div>
     )
 }
