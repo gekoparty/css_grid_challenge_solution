296eda6c99910118010b99aa4ec1166202101cfe
gekoparty <gekoparty@hotmail.com>
2021-08-25 16:09:02 +0200

    made css_grid challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,10 +6,157 @@ import '../styles/index.scss';
 const App = () => {
     return (
         <div>
-            <div class="container">
-            <p id="name">Pumpkin</p>
+            <div className="container">
+                <div className="item item1">
+                    one
+                </div>
+                <div className="item item2">
+                    two
+                </div>
+                <div className="item item3">
+                    three
+                </div>
+                <div className="item item4">
+                    four
+                </div>
+                <div className="item item5">
+                    five
+                </div>
+                <div className="item item6">
+                    six
+                </div>
+                <div className="item item7">
+                   responsive layout
+                </div>
+                <div className="item item8">
+                    eigth
+                </div>
+                <div className="item item9">
+                    nine
+                </div>
+                <div className="item item1">
+                    ten
+                </div>
+                <div className="item item1">
+                    eleven
+                </div>
+                <div className="item item1">
+                    twelve
+                </div>
+            </div>
+            <div className="container2">
+                <div className="item item11">
+                    one
+                </div>
+                <div className="item item21">
+                    two
+                </div>
+                <div className="item item31">
+                    three
+                </div>
+                <div className="item item41">
+                    four
+                </div>
+                <div className="item item51">
+                    five
+                </div>
+                <div className="item item61">
+                    six
+                </div>
+                <div className="item item71">
+                   responsive layout
+                </div>
+                <div className="item item81">
+                    eigth
+                </div>
+                <div className="item item91">
+                    nine
+                </div>
+                <div className="item item13">
+                    ten
+                </div>
+                <div className="item item12">
+                    eleven
+                </div>
+                <div className="item item1">
+                    twelve
+                </div>
+            </div>
+            <div className="container3">
+                <div className="item itemA">
+                    one
+                </div>
+                <div className="item itemB">
+                    two
+                </div>
+                <div className="item itemC">
+                    three
+                </div>
+                <div className="item itemD">
+                    four
+                </div>
+                <div className="item itemE">
+                    five
+                </div>
+                <div className="item itemF">
+                    six
+                </div>
+                <div className="item itemG">
+                   responsive layout
+                </div>
+                <div className="item itemH">
+                    eigth
+                </div>
+                <div className="item itemI">
+                    nine
+                </div>
+                <div className="item itemJ">
+                    ten
+                </div>
+                <div className="item itemK">
+                    eleven
+                </div>
+                <div className="item itemL">
+                    twelve
+                </div>
+            </div>    
+            
+            {/* corner item is one grid item, entire grid is 6 by 5 */}
+            <div className="challenge_container">
+                <div className="red">
+                </div>
+                <div className="orange">
+                </div>
+                <div className="yellow">
+                </div>
+                <div className="green">
+                </div>
+                <div className="gray">
+                </div>    
+                <div className="blue">
+                </div>
+                <div className="purple">
+                </div>
+            </div>
+            <div className="challenge_solution">
+                <div className="red2">
+                </div>
+                <div className="orange2">
+                </div>
+                <div className="yellow2">
+                </div>
+                <div className="green2">
+                </div>
+                <div className="gray2">
+                </div>    
+                <div className="blue2">
+                </div>
+                <div className="purple2">
+                </div>
+            </div>
         </div>
-        </div>
+        
+
     )
 }
 
